7a90f22a8c7d33b5d4c5b8eecf8a75f28a8a62d6
srenegado <scottstationary@gmail.com>
2026-01-08 16:04:03 -0800

    Add basic math functions

---
+++ calc.js
@@ -0,0 +1,32 @@
+let dispOperandA = 0;
+let dispOperandB = 0;
+let dispOperator = '';
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
